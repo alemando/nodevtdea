de9264a72fa314739f699e3564c3bf73f87834a7
Alejandro <alemandoa@gmail.com>
2019-04-14 23:31:53 -0500

    errores

--- src/config/config.js
+++ src/config/config.js
@@ -5,5 +5,8 @@ let urlDB
 if (process.env.NODE_ENV === 'local'){
 	urlDB = 'mongodb://localhost:27017/nodevtdea'
 }
+else {
+	urlDB = 'mongodb+srv://alemando:<Y4IT1tArHX91neYt>@nodevtdea-llnl8.mongodb.net/nodevtdea?retryWrites=true'
+}
 
 process.env.URLDB = urlDB
