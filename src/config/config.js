0791e792a97f63eff2d8022e12dfb29943af264c
Alejandro <alemandoa@gmail.com>
2019-04-14 23:26:39 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'local'
 
-if(!process.env.URLDB){
-
-	process.env.URLDB = 'mongodb://localhost:27017/Cursos'
-	
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/nodevtdea'
 }
+
+process.env.URLDB = urlDB
