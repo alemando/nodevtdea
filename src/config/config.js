5557a3b23b61420063f83b8390f62b19b8792fab
Alejandro <alemandoa@gmail.com>
2019-04-14 23:21:59 -0500

    Seguridad?

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,8 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'local'
 
-let urlDB
-if (process.env.NODE_ENV === 'local'){
-	urlDB = 'mongodb://localhost:27017/nodevtdea'
-}
-else {
-	urlDB = 'mongodb+srv://alemando:<Y4IT1tArHX91neYt>@nodevtdea-llnl8.mongodb.net/nodevtdea?retryWrites=true'
-}
+if(!process.env.URLDB){
 
-process.env.URLDB = urlDB
+	process.env.URLDB = 'mongodb://localhost:27017/Cursos'
+	
+}
