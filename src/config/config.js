b60d2f309335002c09d6f091192742ba843d9017
Alejandro <alemandoa@gmail.com>
2019-04-14 23:47:58 -0500

    Seguridad?

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'local'){
 	urlDB = 'mongodb://localhost:27017/nodevtdea'
 }
 else {
-	urlDB = 'mongodb+srv://alemando:<Y4IT1tArHX91neYt>@nodevtdea-llnl8.mongodb.net/nodevtdea?retryWrites=true'
+	urlDB = 'mongodb+srv://alemando:Y4IT1tArHX91neYt@nodevtdea-llnl8.mongodb.net/nodevtdea?retryWrites=true'
 }
 
 process.env.URLDB = urlDB
